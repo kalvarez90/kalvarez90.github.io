405a36878e5ac30518c50a40ed33e7fe2a69b913
Karina <alvarez.webdeveloper@gmail.com>
2020-10-13 19:54:26 -0400

    'commit'

--- script.js
+++ script.js
@@ -67,21 +67,3 @@ window.addEventListener("load", function() {
   tw.container = document.getElementById(tw.container);
   tw.draw();
 });
-
-
-// READ MORE BUTTON
-function readmore() {
-  var dots = document.getElementById("dots");
-  var moreText = document.getElementById("more");
-  var btnText = document.getElementById("myBtn");
-
-  if (dots.style.display === "none") {
-    dots.style.display = "inline";
-    btnText.innerHTML = "Read more";
-    moreText.style.display = "none";
-  } else {
-    dots.style.display = "none";
-    btnText.innerHTML = "Read less";
-    moreText.style.display = "inline";
-  }
-}
